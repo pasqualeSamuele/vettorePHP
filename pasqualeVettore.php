9af5be144fda884d5b88b3dadd9329703c352fd2
pasqualeSamuele <57179066+pasqualeSamuele@users.noreply.github.com>
2019-10-30 12:47:29 +0100

    Add files via upload

---
+++ pasqualeVettore.php
@@ -0,0 +1,24 @@
+<html>
+<head>
+<title>Quadri</title>
+</head>
+<body>
+<?php
+  
+  //vettore 
+ $profeti = ['Elia', 'Geremia', 'Ezechiele ', 'Daniele', 'Osea', 'Gioele', 'Amos', 'Abdia', 'Giona', 'Michea', 'Naum', 'Abacuc', 'Sofonia', 'Aggeo', 'Zaccaria ', 'Malachia'];
+
+ 
+
+  
+ 
+
+  echo "vettore Profeti :" . '<br>';
+  foreach ($profeti as $item){
+    
+    echo $item . '<br>';
+  }
+
+?>
+</body>
+</html>
